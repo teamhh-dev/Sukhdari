8d7b8cfc37c4cdc3f89462fe7971f7f7a8d1b133
Hamza Ali <teamhh.dev@gmail.com>
2022-01-10 05:28:04 +0500

    Data Class Project +AppDbContext +Connection String+Migration+AppDbContext Configurations Added!

--- Sukhdari_Server/Startup.cs
+++ Sukhdari_Server/Startup.cs
@@ -1,7 +1,9 @@
+using DataAccess.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,9 @@ namespace Sukhdari_Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
