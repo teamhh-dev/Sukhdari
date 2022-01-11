eaeb8c477b3dd9ef42104caf514f8ab69c619322
Hamza Ali <teamhh.dev@gmail.com>
2022-01-11 21:52:33 +0500

    StoreAdminRepo Interface Added and Implemented and project references added in bussiness!

---
+++ Business/StoreAdminRepo.cs
@@ -0,0 +1,39 @@
+﻿using Business.IRepo;
+using Common;
+using DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class StoreAdminRepo : IStoreAdminRepo
+    {
+        //private readonly SignInManager
+
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public StoreAdminRepo(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task<int> SignUpAdmin(StoreSignUpDTO storeSignUpDTO)
+        {
+            IdentityUser user = new IdentityUser() { UserName = storeSignUpDTO.UserName,Email=storeSignUpDTO.Email, PhoneNumber = storeSignUpDTO.phone};
+            await _userManager.CreateAsync(user, storeSignUpDTO.Password);
+            IdentityUser userForRole = await _userManager.FindByEmailAsync(storeSignUpDTO.Email);
+            await _userManager.AddToRoleAsync(userForRole, StaticDetails.Role_StoreAdmin);
+            var id = userForRole.Id;
+            await _db.Stores.AddAsync(new Store() { StoreName = storeSignUpDTO.StoreName, StoreType = storeSignUpDTO.StoreType, Country = storeSignUpDTO.Country, User = id });
+            return  _db.SaveChanges();
+        }
+    }
+}
