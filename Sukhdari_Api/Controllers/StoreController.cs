ade7d05d246e60b89b8c725089343c80fe289bf3
teamhh-dev <teamhh.dev@gmail.com>
2022-01-17 09:42:23 +0500

    Product and store controller added!

---
+++ Sukhdari_Api/Controllers/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using Business.IRepo;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sukhdari_Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class StoreController : Controller
+    {
+        private readonly IStoreRepo _storeRepo;
+        public StoreController(IStoreRepo storeRepo)
+        {
+
+            _storeRepo = storeRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchStore()
+        {
+            var allStores = await _storeRepo.getAllStores();
+;            return Ok(allStores);
+        }
+    }
+}
