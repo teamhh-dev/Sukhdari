cf2008fb02fb2f6fcb28bd0973ff08ba349aca68
teamhh-dev <teamhh.dev@gmail.com>
2022-01-24 02:23:05 +0500

    functions added in storecontroller

--- Sukhdari_Api/Controllers/StoreController.cs
+++ Sukhdari_Api/Controllers/StoreController.cs
@@ -14,11 +14,13 @@ namespace Sukhdari_Api.Controllers
     {
         private readonly IStoreRepo _storeRepo;
         private readonly IProductRepo _productRepo;
-        public StoreController(IStoreRepo storeRepo,IProductRepo productRepo)
+        private readonly ICategoryRepo _categoryRepo;
+        public StoreController(IStoreRepo storeRepo,IProductRepo productRepo, ICategoryRepo categoryRepo)
         {
 
             _storeRepo = storeRepo;
             _productRepo = productRepo;
+            _categoryRepo = categoryRepo;
         }
 
         [HttpGet]
@@ -41,6 +43,21 @@ namespace Sukhdari_Api.Controllers
             var products = await _productRepo.getAllProducts();
             var productsToFind = products.FirstOrDefault(i => i.StoreId == s_Id);
             return Ok(productsToFind);
+        }
+
+        [HttpGet("{CategoryName}")]
+        public async Task<IActionResult> GetStoresByCategory(string categoryName)
+        {
+            var stores = _categoryRepo.getStoreByCategory(categoryName);
+            return Ok(stores);
+        }
+
+        [HttpGet("{PrdouctName}")]
+        public async Task<IActionResult> GetStoresByProduct(string productName)
+        {
+            var stores = _productRepo.getStoresByProductName(productName);
+            
+            return Ok(stores);
         }
     }
 }
