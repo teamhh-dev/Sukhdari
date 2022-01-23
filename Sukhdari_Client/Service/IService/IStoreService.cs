37f077017cef248ed831406061b4a2e9bdb27964
teamhh-dev <teamhh.dev@gmail.com>
2022-01-23 18:33:16 +0500

    Store Service added and iterating store in index

---
+++ Sukhdari_Client/Service/IService/IStoreService.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sukhdari_Client.Service.IService
+{
+    public interface IStoreService
+    {
+        public Task<IEnumerable<StoreDTO>> GetAllStores();
+        //public Task<HotelRoomDTO> GetHotelRoomDetails(int roomId, string checkInDate, string checkOutDate);
+    }
+}
---
+++ Sukhdari_Client/Service/StoreService.cs
@@ -0,0 +1,37 @@
+﻿using Models;
+using Newtonsoft.Json;
+using Sukhdari_Client.Service.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sukhdari_Client.Service
+{
+    public class StoreService : IStoreService
+    {
+        private readonly HttpClient _httpClient;
+        public StoreService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+
+        }
+        public async Task<IEnumerable<StoreDTO>> GetAllStores()
+        {
+            var response = await _httpClient.GetAsync("api/store/getAllStores");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var stores = JsonConvert.DeserializeObject <IEnumerable < StoreDTO >> (content);
+                return stores;
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var errorModel = JsonConvert.DeserializeObject<ErrorModelDTO>(content);
+                throw new Exception(errorModel.ErrorMessage);
+            }
+        }
+    }
+}
