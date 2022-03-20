5356a810a0f93887962bf6b5e5af918b918dfaf5
teamhh-dev <teamhh.dev@gmail.com>
2022-03-21 02:48:56 +0500

    IpAddress class+DTO added ,UserIp repository pattern implementation,Migratons added for ip address and service added to startup!

---
+++ Business/IRepo/IUserIPRepo.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.IRepo
+{
+    public interface IUserIPRepo
+    {
+        public Task<int> createIp(UserIpDTO userIpDTO);
+        public List<int> getPastWeekUniqueUsersCount(int storeId);
+
+    }
+}
