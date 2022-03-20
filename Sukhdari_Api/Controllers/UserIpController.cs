8b87fb5311d1b9a5ff7c7b5d364a52c8374e6521
teamhh-dev <teamhh.dev@gmail.com>
2022-03-21 02:50:47 +0500

    UserIpController implmentation,And adding UserIp service!

---
+++ Sukhdari_Api/Controllers/UserIpController.cs
@@ -0,0 +1,29 @@
+﻿using Business.IRepo;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sukhdari_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+
+    public class UserIpController : Controller
+    {
+        private readonly IUserIPRepo _userIPRepo;
+        public UserIpController(IUserIPRepo userIPRepo)
+        {
+            _userIPRepo = userIPRepo;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create([FromBody] UserIpDTO dto)
+        {
+            var res=await _userIPRepo.createIp(dto);
+            return Ok();
+        }
+    }
+}
